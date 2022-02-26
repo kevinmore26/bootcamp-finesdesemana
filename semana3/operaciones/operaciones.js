7d410f07dd9185aa50f896dade948bbeef44d98e
Sisley More <sdrmy.20@gmail.com>
2022-02-26 10:12:05 -0500

    operaciones

---
+++ semana3/operaciones/operaciones.js
@@ -0,0 +1,40 @@
+let a = 100;
+let b = 4;
+
+let suma = a + b;
+console.log(suma);
+
+let resta = b-a;
+console.log(resta);
+
+let multiplicacion = a * b;
+console.log(multiplicacion);
+
+
+let division = a / b;
+console.log(division);
+
+
+let residuo = 105 % 4;
+console.log(residuo);
+
+//concatenar
+
+let nombre = "kevin";
+let saludo = "hola soy ";
+
+let edad = 80;
+
+let oracion = saludo + nombre + "y tengo " + edad;
+
+console.log(oracion)
+
+console.log("11" + 1) //concatenar
+console.log("11" - 1) //resta
+
+let saludo2 = "Buen día"
+let saludo3 = "gente"
+
+let oracion2 = `${saludo2} ${saludo3} jeje  ` 
+
+console.log(oracion2)
