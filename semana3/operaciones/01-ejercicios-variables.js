6a66ad17d6f676008ebfdd22fb0a694464f9727f
Sisley More <sdrmy.20@gmail.com>
2022-02-26 10:51:51 -0500

    avances prompt

---
+++ semana3/operaciones/01-ejercicios-variables.js
@@ -0,0 +1,18 @@
+// 1- porque pagar por un adivino cuando puedes tener el tuyo propio
+// guardar en variables .
+//A-la cantidad de hijos
+//B- el nombre de su pareja
+//C- localizacion geografica 
+//D-un trabajo
+// Estará casado con ______ tendrás ____ hijos y viviran en _____ y estará trabajando en _______
+
+
+
+//una empresa desea saber la edad de un trabajador pero no saben el año de nacimiento , se asume que el trabajador nació el 01 de enero 
+// qué año nació 
+
+let anioDeNacimiento =  +prompt("qué año nació")
+console.log(2021- anioDeNacimiento)
+
+// TAREAAAAAAAAAAAAAAAAAAA
+//conversor de celcius a farenheit
---
+++ semana3/operaciones/operaciones2.js
@@ -0,0 +1,28 @@
+//alert("hola mundo");
+
+//let nombre = prompt ("Ingresa tu nombre");
+//console.log(nombre);
+
+// todo lo que capture el prompt, lo hará como ""texto"" (string)
+
+//parseInt , convierte un número en formato de texto  a un tipo number
+
+
+//-----------------------------------
+
+//let edad = prompt ("Ingresa tu edad");
+//console.log(parseFloat(edad));
+
+//forma hard
+
+
+//------------------------------------
+
+
+//let edad = +prompt("Ingresa tu edad");
+//console.log(edad)
+
+
+let laQuieres = confirm ("Bro, la quieres?");
+
+console.log(laQuieres)
