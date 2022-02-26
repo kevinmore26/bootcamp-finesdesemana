57056dd3bc640c1bd3332ddcc05631d89b1ceb79
Sisley More <sdrmy.20@gmail.com>
2022-02-26 09:52:06 -0500

    26-02

---
+++ semana3/introjs/intro.js
@@ -0,0 +1,39 @@
+console.log("Hola mundo");
+
+console.warn("HEY, soy una advertencia");
+
+console.error("hola, soy un error");
+
+// var saludo = "holaquetal"
+// console.log(saludo)
+
+let otroSaludo = "hola como estas, recuerda NUNCA usar var a la hora de definir una variable ";
+
+console.log(otroSaludo);
+
+
+//const nombre="pepito";
+
+
+//console.log(nombre);
+
+nombre="juanito";
+
+
+let edadPerro = 15;
+
+let nombreAlternativo ="kev";
+
+console.log(nombreAlternativo);
+
+let edadKev=60;
+console.log(typeof edadKev);
+
+
+let esDeDia= true;
+console.log(esDeDia);
+
+
+let rareza = 1000 / "patatas"
+console.log(rareza)
+
